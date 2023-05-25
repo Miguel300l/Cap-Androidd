1cfa2ee0ae6f2419e472c13b4427a86c8ac040dd
jhoan <jhoan.rko@gmail.com>
2023-05-25 11:06:43 -0500

    hago la validacion de que los campos no esten vacios

--- app/src/main/java/com/example/bottomnavigationdemo/Login2.java
+++ app/src/main/java/com/example/bottomnavigationdemo/Login2.java
@@ -10,25 +10,47 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class Login2 extends AppCompatActivity {
 
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_login2);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
 
-            Button btnConfiguracion = findViewById(R.id.loginButton2);
-            btnConfiguracion.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(Login2.this, MainActivity.class);
-                    startActivity(intent);
+        Button btnConfiguracion = findViewById(R.id.loginButton2);
+        btnConfiguracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Obtener referencias a los campos de texto
+                EditText usernameEditText = findViewById(R.id.usernameEditText);
+                EditText passwordEditText = findViewById(R.id.passwordEditText);
+
+                // Obtener los valores ingresados en los campos de texto
+                String username = usernameEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                // Verificar si los campos están vacíos
+                if (username.isEmpty()) {
+                    usernameEditText.setError("Ingrese un nombre de usuario");
+                    return;
                 }
-            });
-        }
+
+                if (password.isEmpty()) {
+                    passwordEditText.setError("Ingrese una contraseña");
+                    return;
+                }
+
+                // Si los campos no están vacíos, continuar con el inicio de sesión
+                Intent intent = new Intent(Login2.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
 
 
 }
