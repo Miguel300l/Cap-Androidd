643f5bd154971f9896f1809ebcfc7567e40be5e6
jhoan <jhoan.rko@gmail.com>
2023-05-25 10:51:36 -0500

    arreglo el login con su respectivo boton

--- app/src/main/java/com/example/bottomnavigationdemo/Login.java
+++ app/src/main/java/com/example/bottomnavigationdemo/Login.java
@@ -39,4 +39,5 @@ public class Login extends Fragment {
 
         return view;
     }
+
 }
--- app/src/main/java/com/example/bottomnavigationdemo/Login2.java
+++ app/src/main/java/com/example/bottomnavigationdemo/Login2.java
@@ -1,8 +1,10 @@
 package com.example.bottomnavigationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.FragmentManager;
 
-import android.app.FragmentManager;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +12,23 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 public class Login2 extends AppCompatActivity {
+
+
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_login2);
+
+            Button btnConfiguracion = findViewById(R.id.loginButton2);
+            btnConfiguracion.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(Login2.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            });
+        }
 
 
 }
