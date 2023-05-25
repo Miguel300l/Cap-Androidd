351effc17400338f6fd1b9b23e5707980a9d2f9f
jhoan <jhoan.rko@gmail.com>
2023-05-25 12:08:44 -0500

    Hago la funcionalidad del Boton de registro

--- app/src/main/java/com/example/bottomnavigationdemo/Login2.java
+++ app/src/main/java/com/example/bottomnavigationdemo/Login2.java
@@ -2,6 +2,7 @@ package com.example.bottomnavigationdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
@@ -11,6 +12,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login2 extends AppCompatActivity {
 
@@ -21,8 +23,10 @@ public class Login2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
-        Button btnConfiguracion = findViewById(R.id.loginButton2);
-        btnConfiguracion.setOnClickListener(new View.OnClickListener() {
+        //login usuario
+        Button btnLoginUsuario = findViewById(R.id.loginButton2);
+
+        btnLoginUsuario.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // Obtener referencias a los campos de texto
@@ -49,7 +53,26 @@ public class Login2 extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        //aqui ingresamos la funcionalidad del text
+        TextView tvregistrar = findViewById(R.id.textRegistrarD);
+        tvregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Replace the current activity with the Registro fragment
+                Fragment registroFragment = new Registro();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                fragmentManager.beginTransaction()
+                        .replace(android.R.id.content, registroFragment)
+                        .commit();
+            }
+        });
+        //aqui termina la funcionalidad del text
     }
+
+
+
+
 
 
 
--- app/src/main/java/com/example/bottomnavigationdemo/Registro.java
+++ app/src/main/java/com/example/bottomnavigationdemo/Registro.java
@@ -1,5 +1,6 @@
 package com.example.bottomnavigationdemo;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -14,12 +15,20 @@ import android.widget.Button;
 public class Registro extends Fragment {
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_registro, container, false);
 
-
+        Button btnConfiguracion = view.findViewById(R.id.RegisterButton);
+        btnConfiguracion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
 
         return view;
     }
+
+
 }
