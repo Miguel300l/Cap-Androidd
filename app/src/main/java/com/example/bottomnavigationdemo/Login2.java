bef61be3208495ebf579ea22fff0f6efe53a3282
Miguel <miguelledezma400@gmail.com>
2023-05-28 23:13:14 -0500

    Consumo de login y registro

--- app/src/main/java/com/example/bottomnavigationdemo/Login2.java
+++ app/src/main/java/com/example/bottomnavigationdemo/Login2.java
@@ -1,11 +1,13 @@
 package com.example.bottomnavigationdemo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,67 +15,143 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class Login2 extends AppCompatActivity {
 
-
+    EditText loginUsername, loginPassword;
+    Button loginButton;
+    TextView signupRedirectText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
-        //login usuario
-        Button btnLoginUsuario = findViewById(R.id.loginButton2);
+        loginUsername = findViewById(R.id.login_username);
+        loginPassword = findViewById(R.id.login_password);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+        loginButton = findViewById(R.id.login_button);
 
-        btnLoginUsuario.setOnClickListener(new View.OnClickListener() {
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                // Obtener referencias a los campos de texto
-                EditText usernameEditText = findViewById(R.id.usernameEditText);
-                EditText passwordEditText = findViewById(R.id.passwordEditText);
-
-                // Obtener los valores ingresados en los campos de texto
-                String username = usernameEditText.getText().toString().trim();
-                String password = passwordEditText.getText().toString().trim();
-
-                // Verificar si los campos están vacíos
-                if (username.isEmpty()) {
-                    usernameEditText.setError("Ingrese un nombre de usuario");
-                    return;
+            public void onClick(View view) {
+                if (!validateUsername() | !validatePassword()){
+                    // Validación fallida
+                } else {
+                    checkUser();
                 }
+            }
+        });
 
-                if (password.isEmpty()) {
-                    passwordEditText.setError("Ingrese una contraseña");
-                    return;
-                }
-
-                // Si los campos no están vacíos, continuar con el inicio de sesión
-                Intent intent = new Intent(Login2.this, MainActivity.class);
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login2.this, Registro.class);
                 startActivity(intent);
             }
         });
-
-        //aqui ingresamos la funcionalidad del text
-        TextView tvregistrar = findViewById(R.id.textRegistrarD);
-        tvregistrar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Replace the current activity with the Registro fragment
-                Fragment registroFragment = new Registro();
-                FragmentManager fragmentManager = getSupportFragmentManager();
-                fragmentManager.beginTransaction()
-                        .replace(android.R.id.content, registroFragment)
-                        .commit();
-            }
-        });
-        //aqui termina la funcionalidad del text
     }
 
+    public Boolean validateUsername(){
+        String val = loginUsername.getText().toString();
+        if (val.isEmpty()){
+            loginUsername.setError("Username cannot be empty");
+            return false;
+        } else {
+            loginUsername.setError(null);
+            return true;
+        }
+    }
 
+    public Boolean validatePassword(){
+        String val = loginPassword.getText().toString();
+        if (val.isEmpty()){
+            loginPassword.setError("Password cannot be empty");
+            return false;
+        } else {
+            loginPassword.setError(null);
+            return true;
+        }
+    }
 
+    public void checkUser(){
+        String correo = loginUsername.getText().toString().trim();
+        String contrasena = loginPassword.getText().toString().trim();
 
+        OkHttpClient client = new OkHttpClient();
 
+        RequestBody requestBody = new FormBody.Builder()
+                .add("correo", correo)
+                .add("contrasena", contrasena)
+                .build();
 
+        Request request = new Request.Builder()
+                .url("https://backend-cap-273v.vercel.app/loginAprendiz")
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(Login2.this, "Error al enviar la solicitud", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                final String responseBody = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (response.isSuccessful()) {
+                            try {
+                                JSONObject responseJson = new JSONObject(responseBody);
+                                String token = responseJson.getString("token");
+
+                                // Guardar el token en SharedPreferences
+                                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("token", token);
+                                editor.apply();
+
+                                // Iniciar sesión exitoso
+                                Toast.makeText(Login2.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+
+                                // Redirigir a la siguiente actividad
+                                Intent intent = new Intent(Login2.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast.makeText(Login2.this, "Error al parsear la respuesta", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(Login2.this, "Credenciales inválidas", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/example/bottomnavigationdemo/Registro.java
+++ app/src/main/java/com/example/bottomnavigationdemo/Registro.java
@@ -1,34 +1,133 @@
 package com.example.bottomnavigationdemo;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.IOException;
 
-public class Registro extends Fragment {
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.MediaType;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+public class Registro extends AppCompatActivity {
+
+    EditText  signupPassword, signupNombres, signupApellidos,
+            signupTipo, signupNumeroDocumento, signupGenero, signupCorreo, signupNumTelefono,
+            signupProfesion;
+    TextView loginRedirectText;
+    Button signupButton;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_registro, container, false);
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_registro);
 
-        Button btnConfiguracion = view.findViewById(R.id.RegisterButton);
-        btnConfiguracion.setOnClickListener(new View.OnClickListener() {
+        signupPassword = findViewById(R.id.signup_password);
+        signupNombres = findViewById(R.id.signup_nombres);
+        signupApellidos = findViewById(R.id.signup_apellidos);
+        signupTipo = findViewById(R.id.signup_tipo);
+        signupNumeroDocumento = findViewById(R.id.signup_numero_documento);
+        signupGenero = findViewById(R.id.signup_genero);
+        signupCorreo = findViewById(R.id.signup_correo);
+        signupNumTelefono = findViewById(R.id.signup_num_telefono);
+        signupProfesion = findViewById(R.id.signup_profesion);
+        signupButton = findViewById(R.id.signup_button);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+
+        signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), MainActivity.class);
-                startActivity(intent);
+            public void onClick(View view) {
+
+                String contrasena = signupPassword.getText().toString();
+                String nombres = signupNombres.getText().toString();
+                String apellidos = signupApellidos.getText().toString();
+                String tipo = signupTipo.getText().toString();
+                String numeroDocumento = signupNumeroDocumento.getText().toString();
+                String genero = signupGenero.getText().toString();
+                String correo = signupCorreo.getText().toString();
+                String numTelefono = signupNumTelefono.getText().toString();
+                String programa = signupProfesion.getText().toString();
+
+                // Crea el cuerpo de la solicitud POST como formulario multipart
+                RequestBody requestBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("contrasena", contrasena)
+                        .addFormDataPart("nombres", nombres)
+                        .addFormDataPart("apellidos", apellidos)
+                        .addFormDataPart("tipo", tipo)
+                        .addFormDataPart("numeroDocumento", numeroDocumento)
+                        .addFormDataPart("genero", genero)
+                        .addFormDataPart("correo", correo)
+                        .addFormDataPart("numTelefono", numTelefono)
+                        .addFormDataPart("programa", programa)
+                        .build();
+
+                // Crea la solicitud POST a la URL de Vercel
+                Request request = new Request.Builder()
+                        .url("https://backend-cap-273v.vercel.app/registrarAprendiz")
+                        .post(requestBody)
+                        .build();
+
+                // Crea el cliente HTTP
+                OkHttpClient client = new OkHttpClient();
+
+                // Envía la solicitud asíncronamente
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        // Manejo del error en caso de fallo de la solicitud
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(Registro.this, "Error al enviar la solicitud", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        // Manejo de la respuesta de la solicitud
+                        final String responseBody = response.body().string();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (response.isSuccessful()) {
+                                    Toast.makeText(Registro.this, "Te has registrado correctamente", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(Registro.this, com.example.bottomnavigationdemo.Login2.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(Registro.this, "Error al registrar: " + responseBody, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
             }
         });
 
-        return view;
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Registro.this, com.example.bottomnavigationdemo.Login2.class);
+                startActivity(intent);
+            }
+        });
     }
-
-
 }
