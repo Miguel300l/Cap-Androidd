60d700581e56b3fc26f68f6d004602d4d2b012fc
Miguel <miguelledezma400@gmail.com>
2023-05-30 01:40:47 -0500

    Consumo de Profesionales

---
+++ app/src/main/java/com/example/bottomnavigationdemo/LoginProfesional.java
@@ -0,0 +1,168 @@
+package com.example.bottomnavigationdemo;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.FormBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+
+
+public class LoginProfesional extends AppCompatActivity {
+
+    EditText loginUsername, loginPassword;
+    Button loginButton;
+    TextView signupRedirectText;
+    TextView TextoRePro;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+
+        loginUsername = findViewById(R.id.login_username);
+        loginPassword = findViewById(R.id.login_password);
+        signupRedirectText = findViewById(R.id.signupRedirectText);
+        TextoRePro = findViewById(R.id.TextoRePro);
+        loginButton = findViewById(R.id.login_button);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!validateUsername() | !validatePassword()){
+                    // Validación fallida
+                } else {
+                    checkUser();
+                }
+            }
+        });
+
+        signupRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginProfesional.this, Registro.class);
+                startActivity(intent);
+            }
+        });
+
+        TextoRePro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginProfesional.this, RegistroProfesional.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    public Boolean validateUsername(){
+        String val = loginUsername.getText().toString();
+        if (val.isEmpty()){
+            loginUsername.setError("Username cannot be empty");
+            return false;
+        } else {
+            loginUsername.setError(null);
+            return true;
+        }
+    }
+
+    public Boolean validatePassword(){
+        String val = loginPassword.getText().toString();
+        if (val.isEmpty()){
+            loginPassword.setError("Password cannot be empty");
+            return false;
+        } else {
+            loginPassword.setError(null);
+            return true;
+        }
+    }
+
+    public void checkUser(){
+        String correo = loginUsername.getText().toString().trim();
+        String contrasena = loginPassword.getText().toString().trim();
+
+        OkHttpClient client = new OkHttpClient();
+
+        RequestBody requestBody = new FormBody.Builder()
+                .add("correo", correo)
+                .add("contrasena", contrasena)
+                .build();
+
+        Request request = new Request.Builder()
+                .url("https://backend-cap-273v.vercel.app/loginProfesional")
+                .post(requestBody)
+                .build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(@NonNull Call call, @NonNull IOException e) {
+                e.printStackTrace();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Toast.makeText(LoginProfesional.this, "Error al enviar la solicitud", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+
+            @Override
+            public void onResponse(@NonNull Call call, @NonNull Response response) throws IOException {
+                final String responseBody = response.body().string();
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (response.isSuccessful()) {
+                            try {
+                                JSONObject responseJson = new JSONObject(responseBody);
+                                String token = responseJson.getString("token");
+
+                                // Guardar el token en SharedPreferences
+                                SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+                                SharedPreferences.Editor editor = sharedPreferences.edit();
+                                editor.putString("token", token);
+                                editor.apply();
+
+                                // Iniciar sesión exitoso
+                                Toast.makeText(LoginProfesional.this, "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+
+                                // Redirigir a la siguiente actividad
+                                Intent intent = new Intent(LoginProfesional.this, MainActivity.class);
+                                startActivity(intent);
+                                finish();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                Toast.makeText(LoginProfesional.this, "Error al parsear la respuesta", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            Toast.makeText(LoginProfesional.this, "Credenciales inválidas", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/example/bottomnavigationdemo/RegistroProfesional.java
+++ app/src/main/java/com/example/bottomnavigationdemo/RegistroProfesional.java
@@ -1,25 +1,157 @@
 package com.example.bottomnavigationdemo;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.bottomnavigationdemo.model.Programa;
+import com.example.bottomnavigationdemo.network.ApiAprendiz;
+import com.example.bottomnavigationdemo.network.UserService;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MultipartBody;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 
 public class RegistroProfesional extends AppCompatActivity {
 
+    EditText signupPassword, signupNombres, signupApellidos,
+            signupNumeroDocumento, signupCorreo, signupNumTelefono, signupProfesion;
 
-    Button buttonregistrarPro;
+    Spinner signupTipo, signupGenero, signupRol;
+    TextView loginRedirectText;
+    Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_profesional);
 
-        buttonregistrarPro = findViewById(R.id.buttonregistrarPro);
 
+        signupPassword = findViewById(R.id.signup_passwordPro);
+        signupNombres = findViewById(R.id.signup_nombresPro);
+        signupApellidos = findViewById(R.id.signup_apellidosPro);
+        signupNumeroDocumento = findViewById(R.id.signup_numero_documento);
+        signupCorreo = findViewById(R.id.signup_correo);
+        signupNumTelefono = findViewById(R.id.signup_num_telefono);
+        signupButton = findViewById(R.id.buttonregistrarPro);
+        loginRedirectText = findViewById(R.id.loginRedirectText);
+        signupTipo = findViewById(R.id.signup_tipo);
+        signupGenero = findViewById(R.id.signup_genero);
+        signupRol = findViewById(R.id.signupRol);
+        signupProfesion = findViewById(R.id.signupProfesion);
+        // Configurar los adaptadores para los spinners
+        ArrayAdapter<CharSequence> tipoAdapter = ArrayAdapter.createFromResource(this, R.array.tipos_array, android.R.layout.simple_spinner_item);
+        tipoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        signupTipo.setAdapter(tipoAdapter);
 
+        ArrayAdapter<CharSequence> generoAdapter = ArrayAdapter.createFromResource(this, R.array.generos_array, android.R.layout.simple_spinner_item);
+        generoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        signupGenero.setAdapter(generoAdapter);
 
+        ArrayAdapter<CharSequence> rolAdapter = ArrayAdapter.createFromResource(this, R.array.profesion_array, android.R.layout.simple_spinner_item);
+        generoAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        signupRol.setAdapter(rolAdapter);
 
+        signupButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String contrasena = signupPassword.getText().toString();
+                String nombres = signupNombres.getText().toString();
+                String apellidos = signupApellidos.getText().toString();
+                String tipo = signupTipo.getSelectedItem().toString();
+                String numeroDocumento = signupNumeroDocumento.getText().toString();
+                String genero = signupGenero.getSelectedItem().toString();
+                String correo = signupCorreo.getText().toString();
+                String profesion = signupProfesion.getText().toString();
+                String numTelefono = signupNumTelefono.getText().toString();
+                String rol = signupRol.getSelectedItem().toString();
+
+                // Crea el cuerpo de la solicitud POST como formulario multipart
+                RequestBody requestBody = new MultipartBody.Builder()
+                        .setType(MultipartBody.FORM)
+                        .addFormDataPart("contrasena", contrasena)
+                        .addFormDataPart("nombres", nombres)
+                        .addFormDataPart("apellidos", apellidos)
+                        .addFormDataPart("tipo", tipo)
+                        .addFormDataPart("numeroDocumento", numeroDocumento)
+                        .addFormDataPart("genero", genero)
+                        .addFormDataPart("correo", correo)
+                        .addFormDataPart("numTelefono", numTelefono)
+                        .addFormDataPart("rol", rol)
+                        .addFormDataPart("profesion", profesion)
+                        .build();
+
+                // Crea la solicitud POST a la URL de Vercel
+                Request request = new Request.Builder()
+                        .url("https://backend-cap-273v.vercel.app/registrarProfesional")
+                        .post(requestBody)
+                        .build();
+
+                // Crea el cliente HTTP
+                OkHttpClient client = new OkHttpClient();
+
+                // Envía la solicitud asíncronamente
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(Call call, IOException e) {
+                        // Manejo del error en caso de fallo de la solicitud
+                        e.printStackTrace();
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                Toast.makeText(RegistroProfesional.this, "Error al enviar la solicitud", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                    }
+
+                    @Override
+                    public void onResponse(Call call, Response response) throws IOException {
+                        // Manejo de la respuesta de la solicitud
+                        final String responseBody = response.body().string();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (response.isSuccessful()) {
+                                    Toast.makeText(RegistroProfesional.this, "Te has registrado correctamente", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(RegistroProfesional.this, com.example.bottomnavigationdemo.LoginProfesional.class);
+                                    startActivity(intent);
+                                } else {
+                                    Toast.makeText(RegistroProfesional.this, "Error al registrar: " + responseBody, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        loginRedirectText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(RegistroProfesional.this, com.example.bottomnavigationdemo.LoginProfesional.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
